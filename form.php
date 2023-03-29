195d8088c74094289d7ef864c82c8b6c88e7c552
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-03-29 17:19:52 -0300

    editando e atualizando informações

--- form.php
+++ form.php
@@ -1,39 +1,11 @@
 <?php 
+        include 'index.php';
+        //consulta a conexão e insere os dados do banco de dados, se as informações forem válidade, senão mostra a saída de dados retornando um erro na conexão e falha ao fazer o login.
+        if(mysqli_query($con, $sql)){
+            echo "Login criado com sucesso!";
+        }
+        else{
+            echo "Erro ao Tentar fazer o Login!:".mysqli_error($conn);
+        }
 
-    $nome = $_POST['nome'];
-    $idade = $_POST['idade'];
-    $email = $_POST['email'];
-
-
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'cadastro';
-
-
-    $con =  mysqli_connect($servername, $username, $password, $database);
-
-    $sql = "INSERT INTO dados (nome, idade, email) VALUES ('$nome', '$idade', '$email')";
-
-
-    // Verifique se as informações de login são válidas
-    if ($nome == 'nome' && $idade == 'idade' && $email == 'email') {
-        // Se as informações de login forem válidas, defina a variável de sessão 'nome', 'idade', 'email' e redirecione para a página 'index.php'
-        $_SESSION['nome'] = $nome;
-        $_SESSION['idade'] = $idade;
-        $_SESSION['email'] = $username;
-        header('Location: index.php');
-        exit;
-    } else {
-        // Se as informações de login não forem válidas, exiba uma mensagem de erro
-        $error = "As informações foram inválidas";
-    }
-
-
-
-    if(mysqli_query($con, $sql)){
-        echo "Login criado com sucesso!";
-    }else{
-        echo "Erro ao Tentar fazer o Login!:".mysqli_error($conn);
-    }
 ?>
--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 </head>
 <body>
     <h1>Login de Acesso</h1>
+    <?php if (isset($error)): ?>
+        <p><?php echo $error ?></p>
+    <?php endif; ?>
 
-    <form method="POST" action="">
+    <form method="POST" action="form.php">
         <label>Nome: </label>
         <input type="text" name="nome" id="nome" /><br/>
         <label>Idade: </label>
@@ -19,6 +22,50 @@
         <input type="email" name="email" id="email"><br />
         <button id="salvar" value="Enviar" onclick="salvarDados()">Salvar</button>
     </form>
+
+    <?php 
+    
+        $nome = $_POST['nome'];
+        $idade = $_POST['idade'];
+        $email = $_POST['email'];
+
+
+        $servername = 'localhost';
+        $username = 'root';
+        $password = '';
+        $database = 'cadastro';
+
+
+        $con =  mysqli_connect($servername, $username, $password, $database);
+
+        $sql = "INSERT INTO dados (nome, idade, email) VALUES ('$nome', '$idade', '$email')";
+
+   
+        session_start();
+
+        if (isset($_POST['nome']) && isset($_POST['idade']) && isset($_POST['email'])){
+            $nome = $_POST['nome'];
+            $idade = $_POST['idade'];
+            $email = $_POST['email'];
+        // Verifique se as informações de login são válidas
+            if ($nome == 'nome' && $idade == 'idade' && $email == 'email') {
+                // Se as informações de login forem válidas, defina a variável de sessão 'nome', 'idade', 'email' e redirecione para a página 'index.php'
+                $_SESSION['nome'] = $nome;
+                $_SESSION['idade'] = $idade;
+                $_SESSION['email'] = $email;
+                header('Location: exames.php'); //Faz o redirecionamento 
+                exit;
+            }else{
+                // Se as informações de login não forem válidas, exiba uma mensagem de erro
+                  $error = "Informações inválidas";
+            }
+        }
+   
+    ?>
+
+      
+
+
 
     <script src="js/main.js"></script>
 </body>
